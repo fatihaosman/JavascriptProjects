fd990f1e0619ed2838232ddf3ac766ba68ff03b8
fatihaosman <fatihaosman17@gmail.com>
2025-05-22 10:02:15 +0300

    solve api issue

--- QuoteGenerator/script.js
+++ QuoteGenerator/script.js
@@ -1,17 +1,26 @@
-const api_url= "https://api.quotable.io/random";
-const quote=document.querySelector(".quote");
-const author=document.querySelector(".author");
-async function getQuote(url){
-  const response = await fetch(url);
-  var data = await response.json();
-  quote.innerHTML=`"${data.content}"`;
-  author.innerHTML=`-${data.author}`;
-  // console.log(data);
+const api_url = "https://zenquotes.io/api/random";
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+async function getQuote(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    quote.innerHTML = `"${data[0].q}"`;
+    author.innerHTML = `- ${data[0].a}`;
+  } catch (error) {
+    quote.innerHTML = `"Failed to load quote."`;
+    author.innerHTML = `Try again later.`;
+    console.error("Fetch error:", error);
+  }
 }
+
 getQuote(api_url);
 
-
-function tweet(){
-  window.open("https://twitter.com/intent/tweet?text="+ quote.innerHTML+" "+author.innerHTML,
-     "Tweet Window", "width=600,height=400");
+function tweet() {
+  window.open(
+    "https://twitter.com/intent/tweet?text=" + quote.innerText + " " + author.innerText,
+    "Tweet Window",
+    "width=600,height=400"
+  );
 }
