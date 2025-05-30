7a3889121712a6e0ee61ef16edb50a86331ac51a
fatihaosman <fatihaosman17@gmail.com>
2025-05-30 12:47:13 +0300

    added img gallery

---
+++ imageGallery/script.js
@@ -0,0 +1,17 @@
+let scrollContainer = document.querySelector(".gallery");
+let backbtn = document.querySelector("#backbtn");
+let nextbtn = document.querySelector("#nextbtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault(); //will prevent the page from scrolling by default
+  scrollContainer.scrollLeft += e.deltaY; //this will scroll the container by the amount of pixels the mouse wheel moved
+  scrollContainer.style.scrollBehavior = "auto"; //this will make the scroll smooth
+});
+nextbtn.addEventListener("click", ()=>{
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
+})
+backbtn.addEventListener("click", ()=>{
+   scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 900;
+})
