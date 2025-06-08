a2691b4c83fec8385ef5cb94296865a6ef361ab9
fatihaosman <fatihaosman17@gmail.com>
2025-06-08 17:12:24 +0300

    finished text-speech

--- textspeech/script.js
+++ textspeech/script.js
@@ -3,6 +3,14 @@ let speech = new  SpeechSynthesisUtterance();
 let voices =[];
 let voiceSelect = document.querySelector("select");
 
+//this function will populate the voice select
+window.speechSynthesis.onvoiceschanged=()=>{
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0]; // will be the default voice
+  voices.forEach((voice,i)=>(voiceSelect.options[i] = new Option(voice.name,i)));// populate the voice select with the available voices
+
+  voiceSelect.addEventListener("change",()=>speech.voice = voices[voiceSelect.value]);//this is to allow the user to change the voice
+};
 document.querySelector("button").addEventListener("click",()=>{
   speech.text = document.querySelector("textarea").value;
   window.speechSynthesis.speak(speech);
