b64e3e130562afb878b36b5429f7f9e8053acc2c
fatihaosman <fatihaosman17@gmail.com>
2025-06-10 10:40:52 +0300

    solved img anim errorr using lineheight

--- imageAnimation/script.js
+++ imageAnimation/script.js
@@ -4,14 +4,14 @@ var originalImg = document.getElementById("originalImg");
 var line =document.getElementById("line");
 
 //original image width will be equal to that of the container
-originalImg.style.width=ImgBox.offsetWidth+"px";
+originalImg.style.width=ImgBox.offsetWidth + "px";
 
 var leftSpace= ImgBox.offsetLeft; //whitespace on the left from container containing image
 
 ImgBox.onmousemove =function(e){
   var boxwidth= (e.pageX - leftSpace) + "px" ; //boxwidth is the distance inside the container from left to the cursor
   //e.pageX;will get the position of mouse on x axis
-  imgwrap.style.width = boxwidth;
+  imgwrap.style.width = boxwidth; //imgwrap width will be equal to the boxwidth
   line.style.left =boxwidth;
 
 
