43f752cc8401041d853d10a9567ff34e878042a1
fatihaosman <fatihaosman17@gmail.com>
2025-06-13 12:29:20 +0300

    just added the live sites

--- progressbar/script.js
+++ progressbar/script.js
@@ -1,13 +1,30 @@
-let number =document.getElementById("number");
+// let number =document.getElementById("number");
 
-let counter=0;
+// let counter=0;
 
-setInterval(() => {
-  if(counter == 65){
-    clearInterval();
-  }else{
-    counter+=1;
-  number.innerHTML = counter + "%";v
-  }
+// setInterval(() => {
+//   if(counter == 65){
+//     clearInterval();
+//   }else{
+//     counter+=1;
+//   number.innerHTML = counter + "%";v
+//   }
   
-}, 30);
+// }, 30);
+
+let numbers = document.querySelectorAll(".number");
+
+let targets = [65, 80, 45]; // desired percent for each circle
+numbers.forEach((numDiv, index) => {
+  let counter = 0;
+  let target = targets[index];
+
+  let interval = setInterval(() => {
+    if (counter == target) {
+      clearInterval(interval);
+    } else {
+      counter += 1;
+      numDiv.innerHTML = counter + "%";
+    }
+  }, 30);
+});
